24122634e4d325357b1825423449addb678a9a8c
liujian <liujian@longshare.com>
2020-04-02 11:15:55 +0800

    公司@添加登录拦截器

---
+++ src/main/java/com/ecut/frozenpearassistant/config/LoginInterceptorConfigurer.java
@@ -0,0 +1,49 @@
+package com.ecut.frozenpearassistant.config;
+
+import com.ecut.frozenpearassistant.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class LoginInterceptorConfigurer
+	implements WebMvcConfigurer {
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		HandlerInterceptor interceptor
+			= new LoginInterceptor();
+		
+		List<String> excludeList = new ArrayList<>();
+		excludeList.add("/users/reg");
+		excludeList.add("/users/login");
+		excludeList.add("/districts/**");
+		excludeList.add("/products/**");
+		
+		excludeList.add("/register");
+		excludeList.add("/login");
+		excludeList.add("/index");
+		excludeList.add("/product");
+		
+		excludeList.add("/bootstrap/**");
+		excludeList.add("/imgs/**");
+		excludeList.add("/css/**");
+		excludeList.add("/js/**");
+		
+		registry.addInterceptor(interceptor)
+			.addPathPatterns("/**")
+			.excludePathPatterns(excludeList);
+	}
+
+}
+
+
+
+
+
+
+
